0b231661e19626544d4e24f132dced1c2b927bfc
vinee0808 <55697383+vinee0808@users.noreply.github.com>
2019-10-04 17:06:11 +0530

    Delete BookRepo.java

--- src/main/java/com/cg/repo/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cg.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.entity.Book;
-
-public interface BookRepo extends JpaRepository<Book, Integer> {
-
-}
